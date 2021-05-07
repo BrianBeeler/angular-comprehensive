3c40e78c317aca4431046960da2ac93da128ff2b
Brian Beeler <brianlbeeler@gmail.com>
2021-05-06 19:42:53 -0500

    Completed section about binding.

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'root-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
   title = 'my-first-app';
